172398bd558c0561aef03f1e61dc8097a97e3de4
MD TAMIZ UDDIN <tamizrabbi@gmail.com>
2024-12-17 16:17:32 +0600

    Nextjs Blog Site

---
+++ configs/axios.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from "axios";
+
+const URL = "https://blogsite-backend-one.vercel.app/api/v1";
+
+const api = axios.create({
+  baseURL: "https://blogsite-backend-one.vercel.app/api/v1",
+});
+
+export const fetcher = async ({
+  url,
+  method = "Get",
+  headers,
+  body,
+}: {
+  url: string;
+  headers: any;
+  body: any;
+  method: "Get" | "Post" | "Put" | "Delete";
+}) => {
+  try {
+    const response = await fetch(URL + url, {
+      method,
+      headers,
+      body: body ? JSON.stringify(body) : null,
+      cache: "no-cache",
+      next: { tags: [url] },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default api;
